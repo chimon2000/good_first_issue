098b2f0da28a770ca43c44155dfe2802ce6fe0f1
chimon2000 <ryan.c.edge@outlook.com>
2020-05-06 01:47:35 -0400

    refactor: simplify routing

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
+  static Route<dynamic> route() {
+    return MaterialPageRoute(builder: (context) {
+      return AboutPage();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
