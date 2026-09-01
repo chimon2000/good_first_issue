97e3d20ee9c53645e4d71caa2ebce4c878c929ac
MIR-RIAZUL <mirriazul859@gmail.com>
2026-09-01 20:53:05 +0600

    refactor: remove duplicate initial card widget

--- lib/ui/widgets/empty_card.dart
+++ lib/ui/widgets/empty_card.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
 class EmptyCard extends StatelessWidget {
-  const EmptyCard({
-    Key? key,
-  }) : super(key: key);
+  const EmptyCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [],
+        children: [],
       ),
     );
   }
