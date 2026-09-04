f195d83c4eae4dc66575d45f07361b5d7c582df9
chimon2000 <ryan.c.edge@outlook.com>
2026-09-04 16:31:57 -0400

    chore: widen SDK to Dart 3 and refresh dependencies
    - environment.sdk >=3.8.0 in root and packages/github_api (was <3.0.0)
    - Major bumps via pub upgrade: riverpod 3, freezed 3, lints 6,
      mocktail 1, share_plus 13, secure_storage 11, markdown 0.7
    - Remove discontinued pedantic and win32 override
    - Regenerate freezed/json_serializable outputs
    - code_coverage.sh: flutter pub run -> dart run (removed in Dart 3)

--- code_coverage.sh
+++ code_coverage.sh
@@ -5,7 +5,7 @@ echo "Run tests and get coverage"
 flutter test --coverage
 
 echo "Remove coverage from untestable areas"
-flutter pub run remove_from_coverage -f coverage/lcov.info -r '(models.*$)|((main|uikit|bootstrap|freezed|device_info|environment|.g).dart$)'
+dart run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '(models.*$)|((main|uikit|bootstrap|freezed|device_info|environment|.g).dart$)'
 
 if hash lcov-summary 2>/dev/null; then
     echo "Build coverage report for VS Code"
--- lib/models/issue_query_result.freezed.dart
+++ lib/models/issue_query_result.freezed.dart
@@ -1,5 +1,5 @@
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -9,248 +9,284 @@ part of 'issue_query_result.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
-
 /// @nodoc
 mixin _$IssuesQueryResult {
-  List<Issue> get issues => throw _privateConstructorUsedError;
-  int get count => throw _privateConstructorUsedError;
-  int? get maxCount => throw _privateConstructorUsedError;
-  String? get endCursor => throw _privateConstructorUsedError;
-  bool get hasNextPage => throw _privateConstructorUsedError;
-  bool get isFetchingMore => throw _privateConstructorUsedError;
 
-  @JsonKey(ignore: true)
-  $IssuesQueryResultCopyWith<IssuesQueryResult> get copyWith =>
-      throw _privateConstructorUsedError;
+ List<Issue> get issues; int get count; int? get maxCount; String? get endCursor; bool get hasNextPage; bool get isFetchingMore;
+/// Create a copy of IssuesQueryResult
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$IssuesQueryResultCopyWith<IssuesQueryResult> get copyWith => _$IssuesQueryResultCopyWithImpl<IssuesQueryResult>(this as IssuesQueryResult, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is IssuesQueryResult&&const DeepCollectionEquality().equals(other.issues, issues)&&(identical(other.count, count) || other.count == count)&&(identical(other.maxCount, maxCount) || other.maxCount == maxCount)&&(identical(other.endCursor, endCursor) || other.endCursor == endCursor)&&(identical(other.hasNextPage, hasNextPage) || other.hasNextPage == hasNextPage)&&(identical(other.isFetchingMore, isFetchingMore) || other.isFetchingMore == isFetchingMore));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(issues),count,maxCount,endCursor,hasNextPage,isFetchingMore);
+
+@override
+String toString() {
+  return 'IssuesQueryResult(issues: $issues, count: $count, maxCount: $maxCount, endCursor: $endCursor, hasNextPage: $hasNextPage, isFetchingMore: $isFetchingMore)';
+}
+
+
 }
 
 /// @nodoc
-abstract class $IssuesQueryResultCopyWith<$Res> {
-  factory $IssuesQueryResultCopyWith(
-          IssuesQueryResult value, $Res Function(IssuesQueryResult) then) =
-      _$IssuesQueryResultCopyWithImpl<$Res, IssuesQueryResult>;
-  @useResult
-  $Res call(
-      {List<Issue> issues,
-      int count,
-      int? maxCount,
-      String? endCursor,
-      bool hasNextPage,
-      bool isFetchingMore});
-}
+abstract mixin class $IssuesQueryResultCopyWith<$Res>  {
+  factory $IssuesQueryResultCopyWith(IssuesQueryResult value, $Res Function(IssuesQueryResult) _then) = _$IssuesQueryResultCopyWithImpl;
+@useResult
+$Res call({
+ List<Issue> issues, int count, int? maxCount, String? endCursor, bool hasNextPage, bool isFetchingMore
+});
 
+
+
+
+}
 /// @nodoc
-class _$IssuesQueryResultCopyWithImpl<$Res, $Val extends IssuesQueryResult>
+class _$IssuesQueryResultCopyWithImpl<$Res>
     implements $IssuesQueryResultCopyWith<$Res> {
-  _$IssuesQueryResultCopyWithImpl(this._value, this._then);
+  _$IssuesQueryResultCopyWithImpl(this._self, this._then);
 
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
+  final IssuesQueryResult _self;
+  final $Res Function(IssuesQueryResult) _then;
 
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? issues = null,
-    Object? count = null,
-    Object? maxCount = freezed,
-    Object? endCursor = freezed,
-    Object? hasNextPage = null,
-    Object? isFetchingMore = null,
-  }) {
-    return _then(_value.copyWith(
-      issues: null == issues
-          ? _value.issues
-          : issues // ignore: cast_nullable_to_non_nullable
-              as List<Issue>,
-      count: null == count
-          ? _value.count
-          : count // ignore: cast_nullable_to_non_nullable
-              as int,
-      maxCount: freezed == maxCount
-          ? _value.maxCount
-          : maxCount // ignore: cast_nullable_to_non_nullable
-              as int?,
-      endCursor: freezed == endCursor
-          ? _value.endCursor
-          : endCursor // ignore: cast_nullable_to_non_nullable
-              as String?,
-      hasNextPage: null == hasNextPage
-          ? _value.hasNextPage
-          : hasNextPage // ignore: cast_nullable_to_non_nullable
-              as bool,
-      isFetchingMore: null == isFetchingMore
-          ? _value.isFetchingMore
-          : isFetchingMore // ignore: cast_nullable_to_non_nullable
-              as bool,
-    ) as $Val);
-  }
+/// Create a copy of IssuesQueryResult
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? issues = null,Object? count = null,Object? maxCount = freezed,Object? endCursor = freezed,Object? hasNextPage = null,Object? isFetchingMore = null,}) {
+  return _then(_self.copyWith(
+issues: null == issues ? _self.issues : issues // ignore: cast_nullable_to_non_nullable
+as List<Issue>,count: null == count ? _self.count : count // ignore: cast_nullable_to_non_nullable
+as int,maxCount: freezed == maxCount ? _self.maxCount : maxCount // ignore: cast_nullable_to_non_nullable
+as int?,endCursor: freezed == endCursor ? _self.endCursor : endCursor // ignore: cast_nullable_to_non_nullable
+as String?,hasNextPage: null == hasNextPage ? _self.hasNextPage : hasNextPage // ignore: cast_nullable_to_non_nullable
+as bool,isFetchingMore: null == isFetchingMore ? _self.isFetchingMore : isFetchingMore // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
+}
+
+}
+
+
+/// Adds pattern-matching-related methods to [IssuesQueryResult].
+extension IssuesQueryResultPatterns on IssuesQueryResult {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _IssuesQueryResult value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _IssuesQueryResult() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _IssuesQueryResult value)  $default,){
+final _that = this;
+switch (_that) {
+case _IssuesQueryResult():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _IssuesQueryResult value)?  $default,){
+final _that = this;
+switch (_that) {
+case _IssuesQueryResult() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( List<Issue> issues,  int count,  int? maxCount,  String? endCursor,  bool hasNextPage,  bool isFetchingMore)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _IssuesQueryResult() when $default != null:
+return $default(_that.issues,_that.count,_that.maxCount,_that.endCursor,_that.hasNextPage,_that.isFetchingMore);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( List<Issue> issues,  int count,  int? maxCount,  String? endCursor,  bool hasNextPage,  bool isFetchingMore)  $default,) {final _that = this;
+switch (_that) {
+case _IssuesQueryResult():
+return $default(_that.issues,_that.count,_that.maxCount,_that.endCursor,_that.hasNextPage,_that.isFetchingMore);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( List<Issue> issues,  int count,  int? maxCount,  String? endCursor,  bool hasNextPage,  bool isFetchingMore)?  $default,) {final _that = this;
+switch (_that) {
+case _IssuesQueryResult() when $default != null:
+return $default(_that.issues,_that.count,_that.maxCount,_that.endCursor,_that.hasNextPage,_that.isFetchingMore);case _:
+  return null;
+
+}
+}
+
 }
 
 /// @nodoc
-abstract class _$$_IssuesQueryResultCopyWith<$Res>
-    implements $IssuesQueryResultCopyWith<$Res> {
-  factory _$$_IssuesQueryResultCopyWith(_$_IssuesQueryResult value,
-          $Res Function(_$_IssuesQueryResult) then) =
-      __$$_IssuesQueryResultCopyWithImpl<$Res>;
-  @override
-  @useResult
-  $Res call(
-      {List<Issue> issues,
-      int count,
-      int? maxCount,
-      String? endCursor,
-      bool hasNextPage,
-      bool isFetchingMore});
+
+
+class _IssuesQueryResult implements IssuesQueryResult {
+   _IssuesQueryResult({required final  List<Issue> issues, required this.count, this.maxCount, this.endCursor, required this.hasNextPage, this.isFetchingMore = false}): _issues = issues;
+  
+
+ final  List<Issue> _issues;
+@override List<Issue> get issues {
+  if (_issues is EqualUnmodifiableListView) return _issues;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_issues);
+}
+
+@override final  int count;
+@override final  int? maxCount;
+@override final  String? endCursor;
+@override final  bool hasNextPage;
+@override@JsonKey() final  bool isFetchingMore;
+
+/// Create a copy of IssuesQueryResult
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$IssuesQueryResultCopyWith<_IssuesQueryResult> get copyWith => __$IssuesQueryResultCopyWithImpl<_IssuesQueryResult>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _IssuesQueryResult&&const DeepCollectionEquality().equals(other._issues, _issues)&&(identical(other.count, count) || other.count == count)&&(identical(other.maxCount, maxCount) || other.maxCount == maxCount)&&(identical(other.endCursor, endCursor) || other.endCursor == endCursor)&&(identical(other.hasNextPage, hasNextPage) || other.hasNextPage == hasNextPage)&&(identical(other.isFetchingMore, isFetchingMore) || other.isFetchingMore == isFetchingMore));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(_issues),count,maxCount,endCursor,hasNextPage,isFetchingMore);
+
+@override
+String toString() {
+  return 'IssuesQueryResult(issues: $issues, count: $count, maxCount: $maxCount, endCursor: $endCursor, hasNextPage: $hasNextPage, isFetchingMore: $isFetchingMore)';
+}
+
+
 }
 
 /// @nodoc
-class __$$_IssuesQueryResultCopyWithImpl<$Res>
-    extends _$IssuesQueryResultCopyWithImpl<$Res, _$_IssuesQueryResult>
-    implements _$$_IssuesQueryResultCopyWith<$Res> {
-  __$$_IssuesQueryResultCopyWithImpl(
-      _$_IssuesQueryResult _value, $Res Function(_$_IssuesQueryResult) _then)
-      : super(_value, _then);
+abstract mixin class _$IssuesQueryResultCopyWith<$Res> implements $IssuesQueryResultCopyWith<$Res> {
+  factory _$IssuesQueryResultCopyWith(_IssuesQueryResult value, $Res Function(_IssuesQueryResult) _then) = __$IssuesQueryResultCopyWithImpl;
+@override @useResult
+$Res call({
+ List<Issue> issues, int count, int? maxCount, String? endCursor, bool hasNextPage, bool isFetchingMore
+});
 
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? issues = null,
-    Object? count = null,
-    Object? maxCount = freezed,
-    Object? endCursor = freezed,
-    Object? hasNextPage = null,
-    Object? isFetchingMore = null,
-  }) {
-    return _then(_$_IssuesQueryResult(
-      issues: null == issues
-          ? _value._issues
-          : issues // ignore: cast_nullable_to_non_nullable
-              as List<Issue>,
-      count: null == count
-          ? _value.count
-          : count // ignore: cast_nullable_to_non_nullable
-              as int,
-      maxCount: freezed == maxCount
-          ? _value.maxCount
-          : maxCount // ignore: cast_nullable_to_non_nullable
-              as int?,
-      endCursor: freezed == endCursor
-          ? _value.endCursor
-          : endCursor // ignore: cast_nullable_to_non_nullable
-              as String?,
-      hasNextPage: null == hasNextPage
-          ? _value.hasNextPage
-          : hasNextPage // ignore: cast_nullable_to_non_nullable
-              as bool,
-      isFetchingMore: null == isFetchingMore
-          ? _value.isFetchingMore
-          : isFetchingMore // ignore: cast_nullable_to_non_nullable
-              as bool,
-    ));
-  }
+
+
+
 }
-
 /// @nodoc
+class __$IssuesQueryResultCopyWithImpl<$Res>
+    implements _$IssuesQueryResultCopyWith<$Res> {
+  __$IssuesQueryResultCopyWithImpl(this._self, this._then);
 
-class _$_IssuesQueryResult implements _IssuesQueryResult {
-  _$_IssuesQueryResult(
-      {required final List<Issue> issues,
-      required this.count,
-      this.maxCount,
-      this.endCursor,
-      required this.hasNextPage,
-      this.isFetchingMore = false})
-      : _issues = issues;
+  final _IssuesQueryResult _self;
+  final $Res Function(_IssuesQueryResult) _then;
 
-  final List<Issue> _issues;
-  @override
-  List<Issue> get issues {
-    if (_issues is EqualUnmodifiableListView) return _issues;
-    // ignore: implicit_dynamic_type
-    return EqualUnmodifiableListView(_issues);
-  }
-
-  @override
-  final int count;
-  @override
-  final int? maxCount;
-  @override
-  final String? endCursor;
-  @override
-  final bool hasNextPage;
-  @override
-  @JsonKey()
-  final bool isFetchingMore;
-
-  @override
-  String toString() {
-    return 'IssuesQueryResult(issues: $issues, count: $count, maxCount: $maxCount, endCursor: $endCursor, hasNextPage: $hasNextPage, isFetchingMore: $isFetchingMore)';
-  }
-
-  @override
-  bool operator ==(dynamic other) {
-    return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is _$_IssuesQueryResult &&
-            const DeepCollectionEquality().equals(other._issues, _issues) &&
-            (identical(other.count, count) || other.count == count) &&
-            (identical(other.maxCount, maxCount) ||
-                other.maxCount == maxCount) &&
-            (identical(other.endCursor, endCursor) ||
-                other.endCursor == endCursor) &&
-            (identical(other.hasNextPage, hasNextPage) ||
-                other.hasNextPage == hasNextPage) &&
-            (identical(other.isFetchingMore, isFetchingMore) ||
-                other.isFetchingMore == isFetchingMore));
-  }
-
-  @override
-  int get hashCode => Object.hash(
-      runtimeType,
-      const DeepCollectionEquality().hash(_issues),
-      count,
-      maxCount,
-      endCursor,
-      hasNextPage,
-      isFetchingMore);
-
-  @JsonKey(ignore: true)
-  @override
-  @pragma('vm:prefer-inline')
-  _$$_IssuesQueryResultCopyWith<_$_IssuesQueryResult> get copyWith =>
-      __$$_IssuesQueryResultCopyWithImpl<_$_IssuesQueryResult>(
-          this, _$identity);
+/// Create a copy of IssuesQueryResult
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? issues = null,Object? count = null,Object? maxCount = freezed,Object? endCursor = freezed,Object? hasNextPage = null,Object? isFetchingMore = null,}) {
+  return _then(_IssuesQueryResult(
+issues: null == issues ? _self._issues : issues // ignore: cast_nullable_to_non_nullable
+as List<Issue>,count: null == count ? _self.count : count // ignore: cast_nullable_to_non_nullable
+as int,maxCount: freezed == maxCount ? _self.maxCount : maxCount // ignore: cast_nullable_to_non_nullable
+as int?,endCursor: freezed == endCursor ? _self.endCursor : endCursor // ignore: cast_nullable_to_non_nullable
+as String?,hasNextPage: null == hasNextPage ? _self.hasNextPage : hasNextPage // ignore: cast_nullable_to_non_nullable
+as bool,isFetchingMore: null == isFetchingMore ? _self.isFetchingMore : isFetchingMore // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
 }
 
-abstract class _IssuesQueryResult implements IssuesQueryResult {
-  factory _IssuesQueryResult(
-      {required final List<Issue> issues,
-      required final int count,
-      final int? maxCount,
-      final String? endCursor,
-      required final bool hasNextPage,
-      final bool isFetchingMore}) = _$_IssuesQueryResult;
 
-  @override
-  List<Issue> get issues;
-  @override
-  int get count;
-  @override
-  int? get maxCount;
-  @override
-  String? get endCursor;
-  @override
-  bool get hasNextPage;
-  @override
-  bool get isFetchingMore;
-  @override
-  @JsonKey(ignore: true)
-  _$$_IssuesQueryResultCopyWith<_$_IssuesQueryResult> get copyWith =>
-      throw _privateConstructorUsedError;
 }
+
+// dart format on
